cee1aebb549334b4236d7853a8fadfc2cc9423f7
sayali-borse <sayaliborse91@gmail.com>
2026-06-04 00:00:21 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,8 @@
 // const express=required('express')
+
 import express from "express";
+
+const app = express();
+app.listen(5000, () => {
+  console.log("Server started at http://localhost:5000");
+});
